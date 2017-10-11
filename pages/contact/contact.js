7012ec760e48d35a4a523bee041fe2c1202fb8ee
张铭 <zm@zhangmingdeMacBook-Pro.local>
2017-10-11 10:49:59 +0800

    use justify-content: space-between in contact page

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -10,21 +10,21 @@ Page({
         index:0,
         icon: '../../images/icon/wechat.png',
         text: '微信号',
-        marginleft:280,
+        marginleft:-280,
         num:"yanzi056855",
         arrow:false
       }, {
         index: 1,
         icon: '../../images/icon/phone.png',
         text: '客服电话',
-        marginleft: 250,
+        marginleft: -242,
         num:"13775059933",
         arrow: false
       }, {
         index: 2,
         icon: '../../images/icon/addr.png',
         text: '公司地址',
-        marginleft: 180,
+        marginleft: -366,
         num:"",
         arrow: true
       }],
